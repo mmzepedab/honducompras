56fd064f979dd7ded51539f23f0dc6e5ed967c6f
mmzepedab <mmzepedab@gmail.com>
2013-06-04 18:18:10 -0600

    Minor changes on index view
    Removed help text and added some help information about Honducompras

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,12 +6,16 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Bienvenido a <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
+
 <ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+        <li><b>¿Qué es HonduCompras?</b></li>
+        <p align="justify">Mediante el Decreto Ejecutivo 010/2005 promulgado en octubre de 2005, se crea el Sistema de Información de Contratación y Adquisiciones del Estado de Honduras, “HonduCompras”,www.honducompras.gob.hn, el cual es el único medio por el que se difundirá y gestionará, a través de Internet, los procedimientos de contratación que celebren los órganos comprendidos en el ámbito de aplicación de la Ley de Contratación del Estado, para lo cual el Órgano Responsable de su administración establecerá los mecanismos, plazos y condiciones para la incorporación gradual de las entidades al Sistema.</p>
+
+        <li><b>¿Quién es el responsable por administrar HonduCompras?</b></li>
+        <p align="justify">La Oficina Normativa de Contratación y Adquisiciones del Estado (ONCAE) será la entidad responsable por administrar HonduCompras.</p>
+
+	
 </ul>
 
 <p>For more details on how to further develop this application, please read
