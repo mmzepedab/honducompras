275f878f291af9bdc606b09555bc67cfb07cfc06
Marlon Zuniga <marlzzeta@gmail.com>
2013-06-06 11:02:52 -0600

    Hice un cambio en el titulo

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Honducompras Marlzzeta',
+	'name'=>'Honducompras Marlon',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
